79f89785c2656dd803c44f4bedb0de4b856aeb09
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-02-01 12:21:33 -0800

    repro

---
+++ App/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+public class Program
+{
+    [Fact]
+    public static void Main(string[] args)
+    {
+        var th = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+        var client = th.CreateClient();
+        client.GetAsync("http://www.microsoft.com").Wait();
+    }
+
+    public class Startup : IStartup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            return;
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+public class UnitTest
+{
+    [Fact]
+    public void Main()
+    {
+        var th = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+        var client = th.CreateClient();
+    }
+
+    public class Startup : IStartup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            return;
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            return services.BuildServiceProvider();
+        }
+    }
+}
